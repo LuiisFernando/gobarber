8c2a7bd78b359dbbb6358d3d52e3c0d08d537544
Luis Fernando <lsh.fernandors@gmail.com>
2019-09-11 21:38:47 -0300

    implementing method to list appointments and pagination adding validation to add appointments

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,8 +1,39 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+
+        const appoitments = await Appointment.findAll({
+            where: { user_id: req.userId, canceled_at: null },
+            order: ['date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            attributes: ['id', 'date'],
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(appoitments);
+    }
+
     async store(req, res) {
         try {
             const schema = Yup.object().shape({
@@ -25,6 +56,31 @@ class AppointmentController {
                 return res.status(401).json({
                     error: 'You can only create appoitments with providers',
                 });
+            }
+
+            // check for past dates
+            const hourStart = startOfHour(parseISO(date));
+
+            // if date informed is before now
+            if (isBefore(hourStart, new Date())) {
+                return res
+                    .status(400)
+                    .json({ error: 'Past dates are not permitted.' });
+            }
+
+            // check date availability
+            const checkAvailability = await Appointment.findOne({
+                where: {
+                    provider_id,
+                    canceled_at: null,
+                    date: hourStart,
+                },
+            });
+
+            if (checkAvailability) {
+                return res
+                    .status(400)
+                    .json({ error: 'Appointment date is not available' });
             }
 
             const appoitment = await Appointment.create({
