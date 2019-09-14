5235869dcebf95e79e98caf18f2a8b0d61f82fab
Luis Fernando <lsh.fernandors@gmail.com>
2019-09-14 01:58:49 -0300

    including lib nodemailer and setting configs to send email when canceled no methode delete

---
+++ backend/src/config/mail.js
@@ -0,0 +1,11 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+        user: '4d331371021f3e',
+        pass: '3e856096bf5ed3',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreplay@gobarber.com.br>',
+    },
+};
