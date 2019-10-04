f63250e9e892887578df62000c0758a5efc1f089
Luis Fernando <lsh.fernandors@gmail.com>
2019-10-03 22:02:31 -0300

    implementing redux and redux-saga auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Provider } from "react-redux";
 import "./config/ReactotronConfig";
 
 import { Router } from "react-router-dom";
@@ -7,14 +7,18 @@ import history from "./services/history";
 
 import Routes from "./routes";
 
+import store from "./store";
+
 import GlobalStyles from "./styles/global";
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
 
