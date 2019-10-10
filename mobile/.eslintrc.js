03a1e202e18b4df5aeaa8e621ee05ce55baa3761
Luis Fernando <lsh.fernandors@gmail.com>
2019-10-09 21:11:02 -0300

    configuring babel-plugin-root-import to use ~ to suffix src

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -5,27 +5,39 @@ module.exports = {
   extends: ["airbnb", "prettier", "prettier/react"],
 
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
   plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
     "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "react/jsx-filename-extension": [
+      "warn",
+      { extensions: [".js", ".jsx"] }
+    ],
     "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "react/jsx-one-expression-per-line": "off",
     "global-require": "off",
     "react-native/no-raw-text": "off",
-    "no-param-reassign": "off",
     "no-underscore-dangle": "off",
-    camelcase: "off",
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": "off",
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      },
+    },
   },
 };
