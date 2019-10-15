161eef069669b08d6b4b21470c492d7895588c11
Luis Fernando <lsh.fernandors@gmail.com>
2019-10-14 23:31:05 -0300

    styling dashboard with appointment component

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Container, Title, List } from './styles';
+
 import Background from '~/components/Background';
+import Appoitment from '~/components/Appoitment';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appoitment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
