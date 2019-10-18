357268b3239567864d4feb59019b1b4c4da8a255
Luis Fernando <lsh.fernandors@gmail.com>
2019-10-17 21:26:08 -0300

    implementing if tab dashboard is selected to loadAppointments and reset stackNavigator if is blur

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { withNavigationFocus } from 'react-navigation';
 
 import api from '~/services/api';
 
@@ -9,18 +9,20 @@ import { Container, Title, List } from './styles';
 import Background from '~/components/Background';
 import Appoitment from '~/components/Appoitment';
 
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
   const [appointments, setAppointments] = useState([]);
 
+  async function loadAppointments() {
+    const response = await api.get('appointments');
+
+    setAppointments(response.data);
+  }
+
   useEffect(() => {
-    async function loadAppointments() {
-      const response = await api.get('appointments');
-
-      setAppointments(response.data);
+    if (isFocused) {
+      loadAppointments();
     }
-
-    loadAppointments();
-  }, []);
+  }, [isFocused]);
 
   async function handleCancel(id) {
     const response = await api.delete(`appointments/${id}`);
@@ -60,3 +62,5 @@ Dashboard.navigationOptions = {
     <Icon name="event" size={20} color={tintColor} />
   ),
 };
+
+export default withNavigationFocus(Dashboard);
